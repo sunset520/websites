7cad22408938d6ef4a1235a7af15f43c7c41b641
张立豪 <2019819305@qq.com>
2022-07-02 15:10:00 +0800

    zlh

---
+++ js/index.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.4.0/lib/darkmode-js.min.js"></script>
+<script>
+ new Darkmode().showWidget();
+</script>body {
+    background: #222;
+    color: #eee;
+}
